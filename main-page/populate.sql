4896bb37cdd92fc259d9fbd00056b614a6c9f953
ben <bp.miller00@gmail.com>
2017-11-03 13:14:24 -0600

    added populate.sql

---
+++ main-page/populate.sql
@@ -0,0 +1,9 @@
+CREATE TABLE logins(
+	username varchar(24),
+	password varchar(32),
+	userID int
+);
+INSERT INTO logins VALUES ('benmiller', 'password', 1);
+INSERT INTO logins VALUES ('allison', 'password2', 2);
+INSERT INTO logins VALUES ('tanner', 'password', 3);
+INSERT INTO logins VALUES ('yangyang', 'password', 4);
