c843f68d585262ec36ebf582619eba45cbfdefab
Allison Rodenbaugh <alro7383@colorado.edu>
2017-11-06 12:05:03 -0700

    Update SQLInsertHandler.php

--- Milestone3/SQLInsertHandler.php
+++ Milestone3/SQLInsertHandler.php
@@ -6,7 +6,7 @@ $Password = $_REQUEST['Password'];
 
 $connection = @mysqli_connect (localhost,  root, Password, ProjectDB);
 
-$query = "insert into logintable (name, qty, price) values ('$Name','$UserName','$Password')";
+$query = "insert into logintable (name, UserName, Password) values ('$Name','$UserName','$Password')";
 $resultset = mysqli_query($connection,$query);
 if ($resultset){
 echo  "<h1>Inserted successfully into the database</h1>";
